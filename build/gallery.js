cb96c75b0b558d26eab0f39e5a6c967128162c25
Paweł Kudłacik <pakudlacik@gmail.com>
2022-05-16 00:37:58 +0200

    Gallery grid layout

--- build/gallery.js
+++ build/gallery.js
@@ -1,11 +1,56 @@
 const GalleryFunctional = props => {
-  return /*#__PURE__*/React.createElement("div", null, props.gallerySet);
+  const [galleryConfiguration, setGalleryConfiguration] = React.useState({
+    "elements": []
+  });
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const dataUrl = "/resources/galleries/" + props.galleryConfigName;
+      const response = await fetch(dataUrl);
+      const config = await response.json();
+      setGalleryConfiguration(config.gallery);
+    };
+
+    fetchData();
+  }, []);
+  return /*#__PURE__*/React.createElement(GalleryThumbnailsView, {
+    config: galleryConfiguration
+  });
+};
+
+const GalleryThumbnailsView = props => {
+  const galleryElements = props.config.elements.map((e, i) => {
+    return /*#__PURE__*/React.createElement("div", {
+      className: "gallery-grid-box",
+      key: i
+    }, /*#__PURE__*/React.createElement(GalleryThumbnail, {
+      details: e.thumbnail,
+      images: e.images
+    }));
+  });
+  const columnTemplate = "1fr ".repeat(props.config.columns);
+  const rowTemplate = "1fr ".repeat(props.config.rows);
+  return /*#__PURE__*/React.createElement("div", {
+    className: "gallery-grid-container",
+    style: {
+      gridTemplateColumns: columnTemplate,
+      gridTemplateRows: rowTemplate
+    }
+  }, galleryElements);
+};
+
+const GalleryThumbnail = props => {
+  const dataUrl = "/resources/img/" + props.details.img;
+  return /*#__PURE__*/React.createElement("div", {
+    className: "gallery-thumbnail-container"
+  }, /*#__PURE__*/React.createElement("img", {
+    src: dataUrl
+  }));
 };
 
 document.querySelectorAll('.gallery-container').forEach(domContainer => {
   const gallerySet = domContainer.dataset.set;
   const root = ReactDOM.createRoot(domContainer);
   root.render( /*#__PURE__*/React.createElement(GalleryFunctional, {
-    gallerySet: gallerySet
+    galleryConfigName: gallerySet
   }));
 });
--- components/gallery.jsx
+++ components/gallery.jsx
@@ -1,14 +1,52 @@
+
 const GalleryFunctional = (props) => {
+  const [galleryConfiguration, setGalleryConfiguration] = React.useState({ "elements": [] });
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const dataUrl = "/resources/galleries/" + props.galleryConfigName
+      const response = await fetch(dataUrl)
+      const config = await response.json()
+      setGalleryConfiguration(config.gallery)
+    }
+    fetchData()
+  }, [])
+
   return (
-    <div>{props.gallerySet}</div>
+    <GalleryThumbnailsView config={galleryConfiguration}></GalleryThumbnailsView>
   )
 }
+
+const GalleryThumbnailsView = (props) => {
+  const galleryElements = props.config.elements.map((e,i) => {
+
+    return (<div className="gallery-grid-box" key={i}>
+      <GalleryThumbnail details={e.thumbnail} images={e.images} ></GalleryThumbnail>
+    </div>)
+  })
+
+  const columnTemplate = "1fr ".repeat(props.config.columns)
+  const rowTemplate = "1fr ".repeat(props.config.rows)
+
+  return (
+    <div className="gallery-grid-container" style={{ gridTemplateColumns: columnTemplate, gridTemplateRows: rowTemplate }} >
+      {galleryElements}
+    </div>)
+}
+
+
+const GalleryThumbnail = (props) => {
+  const dataUrl = "/resources/img/" + props.details.img
+  return (<div className="gallery-thumbnail-container">
+    <img src={dataUrl}></img>
+  </div>)
+}
+
 
 document.querySelectorAll('.gallery-container')
   .forEach(domContainer => {
     const gallerySet = domContainer.dataset.set;
     const root = ReactDOM.createRoot(domContainer);
     root.render(
-      <GalleryFunctional gallerySet={gallerySet}></GalleryFunctional>
+      <GalleryFunctional galleryConfigName={gallerySet}></GalleryFunctional>
     );
   });
