a0df4e55fb95a7c01b1eb84b7daaf3af933f8f97
Paweł Kudłacik <pakudlacik@gmail.com>
2022-06-11 15:06:54 +0200

    Add seasons panel

---
+++ build/seasons.js
@@ -0,0 +1,63 @@
+function determineCollasedState(openPanelIndex, currentIndex) {}
+
+const Seasons = () => {
+  const [openPanelIndex, setOpenPanelIndex] = React.useState(null);
+  const onPanelClick = React.useCallback(index => setOpenPanelIndex(index));
+  return /*#__PURE__*/React.createElement("div", {
+    className: "seasons-panels-container full-height full-width"
+  }, /*#__PURE__*/React.createElement(SeasonPanel, {
+    key: "0" + openPanelIndex?.toString(),
+    background: "red",
+    index: 0,
+    selectedIndex: openPanelIndex,
+    onClick: onPanelClick
+  }), /*#__PURE__*/React.createElement(SeasonPanel, {
+    key: "1" + openPanelIndex?.toString(),
+    background: "blue",
+    index: 1,
+    selectedIndex: openPanelIndex,
+    onClick: onPanelClick
+  }), /*#__PURE__*/React.createElement(SeasonPanel, {
+    key: "2" + openPanelIndex?.toString(),
+    background: "pink",
+    index: 2,
+    selectedIndex: openPanelIndex,
+    onClick: onPanelClick
+  }), /*#__PURE__*/React.createElement(SeasonPanel, {
+    key: "3" + openPanelIndex?.toString(),
+    background: "orange",
+    index: 3,
+    selectedIndex: openPanelIndex,
+    onClick: onPanelClick
+  }));
+};
+
+const SeasonPanel = props => {
+  const onPanelClick = React.useCallback(() => {
+    if (props.selectedIndex === props.index) {
+      props.onClick(null);
+      return;
+    }
+
+    props.onClick(props.index);
+  }, [props.onClick, props.index, props.selectedIndex]);
+  const style = {
+    backgroundColor: props.background
+  };
+  let stateClass = "seasons-panel-neutral";
+
+  if (props.selectedIndex != null) {
+    stateClass = props.selectedIndex === props.index ? "seasons-panel-expanded" : "seasons-panel-collapsed";
+  }
+
+  return /*#__PURE__*/React.createElement("div", {
+    style: style,
+    className: "seasons-panel " + stateClass,
+    onClick: onPanelClick
+  }, "PANEL PORY ROKU");
+};
+
+document.querySelectorAll('.seasons-container').forEach(domContainer => {
+  const root = ReactDOM.createRoot(domContainer);
+  root.render( /*#__PURE__*/React.createElement(Seasons, null));
+});
